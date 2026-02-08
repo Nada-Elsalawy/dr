4d6bf2ddd81dbbf29aac731a508c2202f4a39bfb
Nada-Elsalawy <nelsalawy@gmail.com>
2026-02-08 14:25:06 +0200

    first comit

--- src/pages/Doctor.jsx
+++ src/pages/Doctor.jsx
@@ -1048,9 +1048,9 @@ const DoctorDashboard = () => {
                 </div>
                 
             </div>
-              <div className='bg-white p-3'> 
-                        <h2> Ai Analysis</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur.</p>
+              <div className='bg-white p-3 sm:w-auto'> 
+                        <h2 className='my-3 text-center'> Ai Analysis</h2>
+                        <p className='my-3 text-center'>Lorem ipsum dolor sit amet consectetur.</p>
                         </div>
             </div>
           </div>
@@ -1140,7 +1140,7 @@ const DoctorDashboard = () => {
                 </div>
                 
             </div>
-              <div className='bg-white p-3'> 
+              <div className='bg-white p-3 sm:w-auto'> 
                         <h2 className='my-3 text-center'> Ai Analysis</h2>
                         <p className='text-center'>Hemoglobin:14.2g/dl(Normal)</p>
                         <p className='text-center'>WBC : 7,500/ul(Normal)</p>
