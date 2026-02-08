3a4199e7a6f6b5670ea367cd88894125d6b748fe
Nada-Elsalawy <nelsalawy@gmail.com>
2026-02-08 03:03:26 +0200

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,18 @@ import Layout from './pages/DrAuthLayOut'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-import SignUp from './pages/SignUp'
-import LoginDr from './pages/LoginDr'
-import Wating from './pages/Wating'
+// import SignUp from './pages/SignUp'
+// import LoginDr from './pages/LoginDr'
+// import Wating from './pages/Wating'
 import Doctor from './pages/Doctor'
 
 const router = createBrowserRouter([
   {path :'' , element: <Layout/>, children:[
     // {index:true, element:<DrHome />},
-{path:"signup",element:<SignUp/>},
-{index:true,element:<LoginDr/>},
-{path:"wating",element:<Wating/>},
-{path:"Dr",element:<Doctor/>},
+// {path:"signup",element:<SignUp/>},
+{index:true,element:<Doctor/>},
+// {path:"wating",element:<Wating/>},
+// {path:"Dr",element:<Doctor/>},
   ]}
 ])
 
